0c2c87776e17d8a129f0aa7e68b94c11bcc58f97
Oisin McLaughlin <oisin_mcl_@hotmail.com>
2022-03-04 21:46:28 +0000

    Update to 0.8.12, updated syntax

--- src/FirstContract.sol
+++ src/FirstContract.sol
@@ -1,17 +1,25 @@
-pragma solidity ^0.4.17;
+//SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.12;
 
 contract Inbox {
     string public message;
     
-    constructor (string initialMessage) public {
+    constructor (string memory initialMessage) {
         message = initialMessage;
     }
 
-    function setMessage (string newMessage) public {
+    function setMessage (string memory newMessage) public {
         message = newMessage;
     }
 
-    function getMessage () public view returns (string){
+    function getMessage () public view returns (string memory){
         return message;
     }    
+
+    function doMath(int a, int b) public pure{
+        a + b;
+        b - a;
+        a * b;
+        a == 0;
+    } 
 }
