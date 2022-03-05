cfa0c7f0f486b9c5c8d577b0b347db7139fec38a
Oisin McLaughlin <oisin_mcl_@hotmail.com>
2022-03-05 00:16:24 +0000

    Update to node project structure

--- src/inbox/contracts/Inbox.sol
+++ src/inbox/contracts/Inbox.sol
@@ -1,25 +1,25 @@
 //SPDX-License-Identifier: UNLICENSED
-pragma solidity ^0.8.12;
+pragma solidity ^0.4.17;
 
 contract Inbox {
     string public message;
     
-    constructor (string memory initialMessage) {
-        message = initialMessage;
+   function Inbox (string _initialMessage) public {
+        message = _initialMessage;
     }
 
-    function setMessage (string memory newMessage) public {
+    function setMessage (string newMessage) public {
         message = newMessage;
     }
 
-    function getMessage () public view returns (string memory){
+    function getMessage () public view returns (string){
         return message;
-    }    
+    }
 
     function doMath(int a, int b) public pure{
         a + b;
         b - a;
         a * b;
         a == 0;
-    } 
+    }
 }
